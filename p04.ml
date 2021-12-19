4d81ce1dded8be154d1515485202a47e31b8bc29
arkulnid <arkulnid@gmail.com>
2021-12-19 15:30:43 +0500

    Renamed all files to more convinient name

--- p04.ml
+++ p04.ml
@@ -1,8 +1,6 @@
 let length (xs : 'a list) : int =
-  let n = 0 in
   let rec helper xs n =
     match xs with
     | [] -> n
     | _ :: tail -> helper tail (n + 1)
-  in
-    helper xs n
+  in helper xs 0
