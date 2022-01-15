b634b1d2881393e587821ffad2f6bb7d872351f8
arkulnid <arkulnid@gmail.com>
2022-01-15 11:38:22 +0500

    Append solution for 23th problem

--- p23.ml
+++ p23.ml
@@ -8,21 +8,16 @@ let rand_select xs n =
     if (check li x) && i > 0 then unique (Random.int (List.length xs)) (i-1) (x::li)
     else if i > 0 then unique (Random.int (List.length xs)) i li
     else li
-  in 
-  let unique_list = unique (Random.int (List.length xs)) n [] in
-  
-  (*
+  in  
   let rec get_element li i =
     match li with
     | [] -> raise Not_found
     | h :: t -> if i != 0 then get_element t (i-1) else h
+  in
+  let unique_list = unique (Random.int (List.length xs)) n [] in
+  let rec aux ind acc =
+    match ind with
+    | [] -> acc
+    | h :: t -> aux t ((get_element xs h) :: acc)
+  in aux unique_list []
   
-  *)
-
-
- (*
-  let rec aux num acc len =
-    if num != 0 then aux (num-1) ((get_element xs (Random.int (len-1)))::acc) len else acc
-  in aux n [] (List.length xs)
-*)
-
