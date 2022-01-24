248629ec0d6a4d371ba359f94ba9fc4e7a520e03
arkulnid <arkulnid@gmail.com>
2022-01-24 14:51:09 +0500

    Append solution for 26th problem

--- p26.ml
+++ p26.ml
@@ -1,19 +1,7 @@
 let extract n xs =
-  let rec get el i li tmp acc = 
-    match li with
-    | [] -> if (List.length tmp) = n then tmp::acc else acc 
-    | h :: t -> if i != n then get el (i+1) t (h::tmp) acc
-                else get el 1 li [el] (tmp::acc)
-  in get "a" 1 ["b"; "c"; "d"] ["a"] [] 
-  (*
-  let rec aux list acc =
-    match list with
-    | [] -> acc 
-    | h :: [] -> acc
-    | h :: t -> aux t ((get h 1 t [h] [])::acc)
-  in aux (List.rev xs) []
-*)
-  
-
-
-
+  let rec aux li acc =
+    if List.length acc = n then [List.rev acc]
+    else match li with
+         | [] -> []
+         | h :: t -> aux t (h::acc) @ aux t acc
+  in aux xs []
